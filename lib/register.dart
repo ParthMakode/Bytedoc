da7490d24f60843db4489d8554b432e46c3e9e23
ParthMakode <makodeparth15@gmail.com>
2022-11-18 18:06:00 +0530

    register done

--- lib/register.dart
+++ lib/register.dart
@@ -1,4 +1,7 @@
+// import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class MyRegister extends StatefulWidget {
   const MyRegister({Key? key}) : super(key: key);
@@ -8,6 +11,52 @@ class MyRegister extends StatefulWidget {
 }
 
 class _MyRegisterState extends State<MyRegister> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+  String popup = "Carefully enter password correctly";
+
+  Future signUp() async {
+    if (_passwordController.text.trim() ==
+        _confirmPasswordController.text.trim()) {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+      popup = "Registered Successfully Please Login";
+      // showDialog(context: context, builder: context => AlertDialog() );
+      // showAlert(context);
+
+    } else {
+      popup = "Password fields do not match.\n Please try again";
+      // showAlert(context);
+    }
+
+    // else
+    //   {
+    //     AlertDialog(
+    //         title: const Text('AlertDialog Title'),
+    // content: SingleChildScrollView(
+    // child: ListBody(
+    // children: const <Widget>[
+    // Text('This is a demo alert dialog.'),
+    // Text('Would you like to approve of this message?'),
+    // ],
+    // ),
+    // ),
+    //   }
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    _nameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -42,6 +91,7 @@ class _MyRegisterState extends State<MyRegister> {
                       child: Column(
                         children: [
                           TextField(
+                            controller: _nameController,
                             style: const TextStyle(color: Colors.black),
                             decoration: InputDecoration(
                                 enabledBorder: OutlineInputBorder(
@@ -63,9 +113,10 @@ class _MyRegisterState extends State<MyRegister> {
                                 )),
                           ),
                           const SizedBox(
-                            height: 30,
+                            height: 20,
                           ),
                           TextField(
+                            controller: _emailController,
                             style: const TextStyle(color: Colors.black),
                             decoration: InputDecoration(
                                 enabledBorder: OutlineInputBorder(
@@ -87,9 +138,10 @@ class _MyRegisterState extends State<MyRegister> {
                                 )),
                           ),
                           const SizedBox(
-                            height: 30,
+                            height: 20,
                           ),
                           TextField(
+                            controller: _passwordController,
                             style: const TextStyle(color: Colors.black),
                             obscureText: true,
                             decoration: InputDecoration(
@@ -112,9 +164,10 @@ class _MyRegisterState extends State<MyRegister> {
                                 )),
                           ),
                           const SizedBox(
-                            height: 30,
+                            height: 20,
                           ),
                           TextField(
+                            controller: _confirmPasswordController,
                             style: const TextStyle(color: Colors.black),
                             obscureText: true,
                             decoration: InputDecoration(
@@ -140,15 +193,15 @@ class _MyRegisterState extends State<MyRegister> {
                           //this sized box is for creating space unlike my team's duelists
 
                           const SizedBox(
-                            height: 41,
+                            height: 30,
                           ),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
                               const Text(
-                                'Sign Up',
+                                ' Sign Up',
                                 style: TextStyle(
-                                    color: Colors.white,
+                                    color: Colors.blueGrey,
                                     fontSize: 27,
                                     fontWeight: FontWeight.w700),
                               ),
@@ -157,7 +210,13 @@ class _MyRegisterState extends State<MyRegister> {
                                 backgroundColor: const Color(0xff4c505b),
                                 child: IconButton(
                                     color: Colors.white,
-                                    onPressed: () {},
+                                    // onPressed: signUp,
+                                    onPressed: () {
+                                      signUp();
+                                      showDialog(context: context,builder: (context)=>AlertDialog(title:Text(popup)));
+                                      FirebaseAuth.instance.signOut();
+                                      dispose();
+                                    },
                                     icon: const Icon(
                                       Icons.arrow_forward,
                                     )),
@@ -165,7 +224,7 @@ class _MyRegisterState extends State<MyRegister> {
                             ],
                           ),
                           const SizedBox(
-                            height: 40,
+                            height: 10,
                           ),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -179,11 +238,14 @@ class _MyRegisterState extends State<MyRegister> {
                                   'Sign In',
                                   textAlign: TextAlign.left,
                                   style: TextStyle(
-                                      decoration: TextDecoration.underline,
-                                      color: Colors.white,
-                                      fontSize: 18),
+                                      // decoration: TextDecoration.underline,
+                                      color: Colors.blueGrey,
+                                      fontSize: 27,
+                                      fontWeight: FontWeight.w700),
                                 ),
-                              ),
+
+                                ),
+
                             ],
                           )
                         ],
